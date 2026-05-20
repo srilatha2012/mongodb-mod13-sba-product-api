6c5c63aaa0bf341f5304527568b86f5450055859
srilatha2012 <srilathp2012@gmail.com>
2026-05-20 13:14:37 -0400

    Implemented create product route and fixed the database name in .env file

---
+++ routes/productRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+//create a new router object for product routes
+const productRouter = express.Router();
+
+const Product = require("../models/Product");
+
+//CREATE POST - 
+productRouter.post("/", async (req,res) => {
+    try {
+       const newProduct = await Product.create(req.body);
+       res.status(201).json(newProduct);
+    } catch(error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+module.exports = productRouter;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 //Import the database connection function
 const connectDB = require("./db/connection");
+const productRouter = require("./routes/productRoutes")
 
 //create express application
 const app = express();
@@ -17,8 +18,11 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 //MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //ROUTES
+app.use("/api/product", productRouter);
 
 //PORT
 app.listen(PORT, ()=>{
