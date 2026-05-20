5718ce946991ea6c1ebe5733900c843cdb4dd6dd
srilatha2012 <srilathp2012@gmail.com>
2026-05-20 16:03:56 -0400

    Implemented findById and update request for product api

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,13 +5,13 @@ const productRouter = express.Router();
 
 const Product = require("../models/Product");
 
-//CREATE POST - 
+//CREATE POST - /api/product
 productRouter.post("/", async (req, res) => {
     try {
         const newProduct = await Product.create(req.body);
-         res.status(201).json(newProduct);
+        res.status(201).json(newProduct);
     } catch (error) {
-        if(error.name === "ValidationError") {
+        if (error.name === "ValidationError") {
             return res.status(400).json({
                 message: error.message
             })
@@ -19,5 +19,37 @@ productRouter.post("/", async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+
+// READ ONE -GET - /api/product/:id
+productRouter.get("/:id", async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(400).json({
+                message: "Product not found"
+            })
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+
+})
+
+//update -PUT -/api/product/:id
+productRouter.put("/:id", async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            //show updated data and check validation rules
+            { new: true, runValidators: true }
+        );
+        res.send(updatedProduct)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+
+})
 
 module.exports = productRouter;
